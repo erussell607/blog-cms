8c6674b93a79c597c71d8e37ee70e1381cd43966
Ethan Russell <Ethan.Russell@mclaneco.com>
2019-11-24 21:34:13 -0500

    Finsihed tutorial
    https://www.djamware.com/post/5d88cb43e7939eec17dc4c89/mean-stack-angular-8-tutorial-build-a-simple-blog-cms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,89 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './auth/login/login.component';
+import {RegisterComponent} from './auth/register/register.component';
+import {HomeComponent} from './home/home.component';
+import {AdminComponent} from './admin/admin.component';
+import {BycategoryComponent} from './bycategory/bycategory.component';
+import {DetailsComponent} from './details/details.component';
+import {CategoryComponent} from './category/category.component';
+import {CategoryDetailsComponent} from './category/category-details/category-details.component';
+import {CategoryAddComponent} from './category/category-add/category-add.component';
+import {CategoryEditComponent} from './category/category-edit/category-edit.component';
+import {PostComponent} from './post/post.component';
+import {PostDetailsComponent} from './post/post-details/post-details.component';
+import {PostAddComponent} from './post/post-add/post-add.component';
+import {PostEditComponent} from './post/post-edit/post-edit.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {TokenInterceptor} from './interceptors/token.interceptor';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatMenuModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSortModule,
+  MatTableModule,
+  MatToolbarModule
+} from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {CKEditorModule} from 'ckeditor4-angular';
+import {MatContenteditableModule} from 'mat-contenteditable';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    AdminComponent,
+    BycategoryComponent,
+    DetailsComponent,
+    CategoryComponent,
+    CategoryDetailsComponent,
+    CategoryAddComponent,
+    CategoryEditComponent,
+    PostComponent,
+    PostDetailsComponent,
+    PostAddComponent,
+    PostEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatMenuModule,
+    MatToolbarModule,
+    CKEditorModule,
+    MatContenteditableModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,56 @@
+import {Injectable} from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanActivateChild,
+  CanLoad,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlSegment,
+  UrlTree
+} from '@angular/router';
+import {Observable} from 'rxjs';
+import {AuthService} from "../services/auth.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
+
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+    const url: string = state.url;
+    return this.checkLogin(url);
+  }
+
+  canActivateChild(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+    return true;
+  }
+
+  checkLogin(url: string): boolean {
+    if (this.authService.loggedInStatus) {
+      return true;
+    }
+
+    // Store the attempted URL for redirecting
+    this.authService.redirectUrl = url;
+
+    // Navigate to the login page with extras
+    this.router.navigate(['/login']);
+    return false;
+  }
+}
